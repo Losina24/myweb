31082f29010ae7cc5a6bc1e8b406a7373dfd0507
Alejandro Losa <60261013+Losina24@users.noreply.github.com>
2021-04-24 21:34:57 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -19,14 +19,14 @@
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'hatsukimanga@gmail.com';                  // SMTP username
-        $mail->Password   = 'Jamuchilo1';                 // SMTP password
+        $mail->Username   = 'email@email.com';                  // SMTP username
+        $mail->Password   = 'password';                 // SMTP password
         $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
 
         //Recipients
-        $mail->setFrom('losinagfx@gmail.com', $name);
-        $mail->addAddress('losina.dd@gmail.com', $name);                     // Add a recipient
+        $mail->setFrom('email@email', $name);
+        $mail->addAddress('email@email', $name);                     // Add a recipient
 
         // Content
         $mail->isHTML(true);                                  // Set email format to HTML
